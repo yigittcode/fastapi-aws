4aab621131c49e0bf37bcd199360811ec2f66c25
yigittcode <yigitabdullah329@gmail.com>
2025-10-25 17:24:24 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
